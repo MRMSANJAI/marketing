fb251e6d151885bbfc0b4ffc57960cbf2c775824
yoganathanp <109456314+yoganathanp@users.noreply.github.com>
2023-12-11 16:00:19 +0530

    changes

--- src/Pages/companydetails/Company.jsx
+++ src/Pages/companydetails/Company.jsx
@@ -3,7 +3,12 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import { MdArrowDropDown } from "react-icons/md";
 import { FaFacebook,FaTwitter ,FaLinkedinIn ,FaLink } from "react-icons/fa";
 import "./Company.css";
+import { Tab, Row, Col, Nav } from 'react-bootstrap';
+
+
+
 const Company = () => {
+
   return (
     <div className='company-details'>
         <div className='company-container'>
@@ -70,8 +75,55 @@ const Company = () => {
                   <p>1912</p>
                 </div>
         </div>
-
-    </div>
+        <div className='Ttable-container'>
+           <div className='Tab-content'>
+             <h3>insights</h3> 
+             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+             <Row>
+                <Col sm={3}>
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item >
+              <Nav.Link eventKey="first">Techonologies</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Funding Rounds</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="third">Job Posting</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="fourth">News</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="fifth">Employee Trend</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+          <Tab.Pane eventKey="first">
+                <p>Techonology insight Settings</p>
+                <p>Ai</p>
+                <p>CloudFare hosting</p>
+                <p>CloudFare DNS</p>
+                <p>Facebook Custom Audience</p>
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+              <div>
+                <p>Funding Round Insights</p>
+                <p>Insight 1</p>
+                <p>Insight 2</p>
+              </div>
+            </Tab.Pane>
+          </Tab.Content>
+         
+        </Col>
+      </Row>
+    </Tab.Container>
+  </div>
+  </div> 
+</div>
+    
   );
 }
 
