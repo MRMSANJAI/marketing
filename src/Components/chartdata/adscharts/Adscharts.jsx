1754bfb854ebc7bb8f751ed9dc3db18eeda6e1ef
yoganathanp <109456314+yoganathanp@users.noreply.github.com>
2023-12-12 09:19:07 +0530

    charts

---
+++ src/Components/chartdata/adscharts/Adscharts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Adscharts = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Adscharts;
---
+++ src/Components/chartdata/demographicchart/Demographicchart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demographicchart = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Demographicchart;
