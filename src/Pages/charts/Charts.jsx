f6267c3f86334617a77fc8928b98bf8375841a44
Your Name <selvagopi2@gmail.com>
2023-12-12 10:15:58 +0530

    charts

--- src/Pages/charts/Charts.jsx
+++ src/Pages/charts/Charts.jsx
@@ -6,11 +6,11 @@ const Charts = () => {
       <div className='nav-bar-cont'>
         <nav>
           <ul className='ord-lis-nav'>
-            <li className='camp-list'>Campaign</li>
-            <li className='camp-list'>AdSets</li>
-            <li className='camp-list'>Ads</li>
-            <li className='camp-list'>Demographic</li>
-            <li className='camp-list'>Custom Conversions</li>
+            <li href = "/campaigncharts"className='camp-list'>Campaign</li>
+            <li href = "/adsetscharts"className='camp-list'>AdSets</li>
+            <li href = "/adscharts"className='camp-list'>Ads</li>
+            <li href = "/demographiccharts"className='camp-list'>Demographic</li>
+            <li href = "/customconversionscharts"className='camp-list'>Custom Conversions</li>
           </ul>
         </nav>
       </div>
