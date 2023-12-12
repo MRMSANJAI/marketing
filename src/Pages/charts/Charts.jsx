c316dc329f3f262b1cc2b2e10dc3e458945c06bd
yoganathanp <109456314+yoganathanp@users.noreply.github.com>
2023-12-12 10:32:29 +0530

    changes

--- src/Pages/charts/Charts.jsx
+++ src/Pages/charts/Charts.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import "./Charts.css";
+import { Link } from 'react-router-dom';
 const Charts = () => {
   return (
     <div>
       <div className='nav-bar-cont'>
         <nav>
           <ul className='ord-lis-nav'>
-            <li href = "/campaigncharts"className='camp-list'>Campaign</li>
-            <li href = "/adsetscharts"className='camp-list'>AdSets</li>
-            <li href = "/adscharts"className='camp-list'>Ads</li>
-            <li href = "/demographiccharts"className='camp-list'>Demographic</li>
-            <li href = "/customconversionscharts"className='camp-list'>Custom Conversions</li>
+            <Link to= "/campaignscharts"className='camp-list'>Campaign</Link>
+            <Link to = "/adsetscharts"className='camp-list'>AdSets</Link>
+            <Link to = "/adscharts"className='camp-list'>Ads</Link>
+            <Link to = "/demographiccharts"className='camp-list'>Demographic</Link>
+            <Link href = "/customconversionscharts"className='camp-list'>Custom Conversions</Link>
           </ul>
         </nav>
       </div>
